5daaabd6ea591e9bcf6e8b0cb0867658095d5e7c
hlacis <jakubhlacik@seznam.cz>
2026-04-25 11:09:58 +0200

    Commit 1.6 - sidebar can be hidden now

---
+++ src/main/java/dk/easv/gui/UserviewController.java
@@ -0,0 +1,72 @@
+package dk.easv.gui;
+
+import dk.easv.bll.TIFFService;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+public class UserviewController {
+
+    private final TIFFService apiService = new TIFFService();
+
+    @FXML
+    private VBox sidebar;
+
+    @FXML
+    private Pane sidebarTrigger;
+
+    private boolean sidebarVisible = false;
+
+
+    @FXML
+    private Button sidebarLockButton;
+
+    private boolean sidebarLocked = false;
+
+    @FXML
+    private void initialize() {
+        sidebarTrigger.setOnMouseEntered(event -> showSidebar());
+        sidebar.setOnMouseExited(event -> hideSidebar());
+    }
+
+    private void showSidebar() {
+        if (sidebarVisible) return;
+
+        sidebarVisible = true;
+
+        TranslateTransition transition = new TranslateTransition(Duration.millis(110), sidebar);
+        transition.setToX(180);
+        transition.play();
+    }
+
+    private void hideSidebar() {
+        if (!sidebarVisible || sidebarLocked) return;
+
+        sidebarVisible = false;
+
+        TranslateTransition transition = new TranslateTransition(Duration.millis(220), sidebar);
+        transition.setToX(0);
+        transition.play();
+    }
+
+    @FXML
+    private void onSidebarLockClicked() {
+        sidebarLocked = !sidebarLocked;
+
+        if (sidebarLocked) {
+            if (!sidebarLockButton.getStyleClass().contains("sidebar-icon-button-active")) {
+                sidebarLockButton.getStyleClass().add("sidebar-icon-button-active");
+            }
+            showSidebar();
+        } else {
+            sidebarLockButton.getStyleClass().remove("sidebar-icon-button-active");
+        }
+    }
+
+    public void onFetchFilesClicked() {
+        System.out.println(apiService.getCount());
+    }
+}
